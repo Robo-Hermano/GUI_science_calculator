a88256650c6727395cab837f6674b4ce17d706fa
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-09-30 17:20:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 import "NEA-science_GUI-calculator/constants.py" as CONSTANTS-FILE
+constants_list = CONSTANTS-FILE.dictionary
+
+class Calculator:
+  def __init__(self):
+    master = tk.Tk()
