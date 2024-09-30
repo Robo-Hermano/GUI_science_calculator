60427790f2fb5caf317dcda4839ae36957388abe
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-09-30 09:49:17 +0100

    Create constants.cpp

---
+++ constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+unordered_map <string, int> dictionary;
+dictionary["Avogrado's Constant"] = 6.022*10^23;
+return 0;
+}
