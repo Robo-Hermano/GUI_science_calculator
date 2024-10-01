594e9d8d2fe768617f2f52d76fc7020788ac1cff
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-01 15:06:14 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,20 @@
 dictionary = {}
 dictionary["Avogrado constant"] = 6.022*(10**23)
 dictionary["Boltzmann constant"] = 1.380649*(10**-23)
+dictionary["Gravitational constant"] = 6.67430*(10**-11)
+dictionary["Speed of light"] = 3*(10**8)
+dictionary["Magnetic constant"] = 1.25663706*(10**-6)
+dictionary["Electric constant"] = 8.8541878*(10**-12)
+dictionary["Characteristic impedance of vacuum"] = 376.7303135
+dictionary["Planck constant"] = 6.62607*(10**-34)
+dictionary["Planck mass"] = 2.17647*(10**-8)
+dictionary["Planck length"] = 1.616229*(1-**-35)
+dictionary["Planck time"] = 5.39116*(10**-44)
+
+dictionary["Elementary charge"] = 1.6021766*(10**-19)
+dictionary["Magnetic flux quantum"] = 2.0678338*(10**-15)
+dictionary["Conductance quantum"] = 7.74809173*(10**-5)
+dictionary["Josephson constant"] = 4.83597852*(10**14)
+dictionary["von Klitzing constant"] = 25812.80746
+dictionary["Bohr magneton"] = 9.2740099*(10**-24)
+dictionary["Nuclear magneton"] = 5.0507836*(10**-27)
