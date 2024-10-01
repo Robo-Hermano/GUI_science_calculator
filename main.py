fade40d7a4ece37d0f57e28bf6bb3e42f9d0eced
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-01 15:07:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import "NEA-science_GUI-calculator/constants.py" as CONSTANTS-FILE
+import "NEA-science_GUI-calculator/constants_and_sequences.py" as CONSTANTS-FILE
 constants_list = CONSTANTS-FILE.dictionary
 
 class Calculator:
