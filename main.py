a495acda87c58a4c3631ed6600d49b6119ae089e
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-02 11:30:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 import "NEA-science_GUI-calculator/infos.py" as INFO-FILE
 CONSTANTS = INFO-FILE.dictionary
+TESTS = INFO-FILE.molecule_tests
+AMINOS = INFO-FILE.amino_acids
 
 class Calculator:
   def __init__(self):
