215aa14fef97bb2579ac0f58031637432599d93c
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-02 12:48:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import "NEA-science_GUI-calculator/infos.py" as INFO-FILE
-CONSTANTS = INFO-FILE.dictionary
+CONSTANTS = INFO-FILE.constants_dictionary
 TESTS = INFO-FILE.molecule_tests
 AMINOS = INFO-FILE.amino_acids
 
