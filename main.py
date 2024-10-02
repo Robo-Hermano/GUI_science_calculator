5038a31179ff815014f732d560084efdea20f8bd
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-02 12:53:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ CONSTANTS = INFO-FILE.constants_dictionary
 TESTS = INFO-FILE.molecule_tests
 AMINOS = INFO-FILE.amino_acids
 
-class Stack:
-  #just set up stacks for undoing and redoing stuff in basic calculator operations
-
 class Calculator:
   def __init__(self):
     master = tk.Tk()
