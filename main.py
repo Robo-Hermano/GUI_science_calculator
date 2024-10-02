6b4b6f21af52b105bc8ba4ee8b3090fabce0774f
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-02 13:08:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ CONSTANTS = INFO-FILE.constants_dictionary
 TESTS = INFO-FILE.molecule_tests
 AMINOS = INFO-FILE.amino_acids
 
-class Stack: #for undo and redo
-  def peek(self):
-    return self[len(self)-1]
+#might use for undo and redo
+#class Stack:
+  #def peek(self):
+  #  return self[len(self)-1]
   
-  def push(self, value):
-    self = self.append(value)
-    return self
+  #def push(self, value):
+  #  self = self.append(value)
+  #  return self
 
-  def pop(self):
-    self = self.remove(self[len(self)-1])
-    return self
+  #def pop(self):
+  #  self = self.remove(self[len(self)-1])
+  #  return self
 
 class Calculator:
   def __init__(self):
