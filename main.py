0db9018b60bcb20417e0c4a7001ea601bae92200
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-14 09:29:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 import math
 import "NEA-science_GUI-calculator/infos.py" as INFO-FILE
 CONSTANTS = INFO-FILE.constants_dictionary
@@ -39,7 +38,35 @@ class Sci_Calculator:
     self.master.title("Science Calculator")
     self.navigation()
   def navigation(self):
-    pass #display main options
-
+    self.intro = tk.Label(self.master, text = "MAIN MENU", font = ("Times", 20))
+    self.intro.pack()
+    self.buttonframe = tk.Frame(self.master)
+    self.physics = tk.Button(self.buttonframe, text = "Physics", command = self.physics_menu)
+    self.physics.grid(row = 0, column = 0)
+    self.chemistry = tk.Button(self.buttonframe, text = "Chemistry", command = self.chem_menu)
+    self.chemistry.grid(row = 0, column = 1)
+    self.biology = tk.Button(self.buttonframe, text = "Biology", command = self.bio_menu)
+    self.biology.grid(row = 0, column = 2)
+    self.operations = tk.Button(self.buttonframe, text = "Operations", command = self.calculations)
+    self.operations.grid(row = 1, column = 0)
+    self.constants = tk.Button(self.buttonframe, text = "Display Constants", command = self.display_constants)
+    self.constants.grid(row = 1, column = 1)
+    self.units = tk.Button(self.buttonframe, text = "Convert Units", command = self.conversions)
+    self.units.grid(row = 1, column = 2)
+    self.graphs = tk.Button(self.buttonframe, text = "Datasheet and Graphs", command = self.data_graphs)
+  def physics_menu(self):
+    self.buttonframe.forget()
+  def chem_menu(self):
+    self.buttonframe.forget()
+  def bio_menu(self):
+    self.buttonframe.forget()
+  def calculations(self):
+    self.buttonframe.forget()
+  def display_constants(self):
+    self.buttonframe.forget()
+  def conversions(self):
+    self.buttonframe.forget()
+  def data_graphs(self):
+    self.buttonframe.forget()
 gui_calc = Sci_Calculator()
 gui_calc.master.mainloop()
