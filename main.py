ff8e6cc7d86ca7c7cea7fbc5c7096c43d3db0a75
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-10-14 09:46:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class Sci_Calculator:
     self.master.geometry("500x500")
     self.master.state("zoomed")
     self.master.title("Science Calculator")
-    self.navigation()
-  def navigation(self):
+    self.main_menu()
+  def main_menu(self):
     self.intro = tk.Label(self.master, text = "MAIN MENU", font = ("Times", 20))
     self.intro.pack()
     self.buttonframe = tk.Frame(self.master)
@@ -54,19 +54,65 @@ class Sci_Calculator:
     self.units = tk.Button(self.buttonframe, text = "Convert Units", command = self.conversions)
     self.units.grid(row = 1, column = 2)
     self.graphs = tk.Button(self.buttonframe, text = "Datasheet and Graphs", command = self.data_graphs)
+    self.graphs.grid(row = 2, column = 1)
+    
   def physics_menu(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
+    self.physics_frame = tk.Frame(self.master)
+    display = tk.Label(self.physics_frame, text = "PHYSICS MENU", font = ("Times", 20))
+    display.grid(row = 0, column = 1)
+    self.suvat_calcs = tk.Button(self.physics_frame, text = "S.U.V.A.T. calculations", command = self.suvat)
+    self.suvat_calcs.grid(row = 1, column = 0)
+    self.rays = tk.Button(self.physics_frame, text = "Ray diagrams", command = self.ray_diagrams)
+    self.rays.grid(row = 1, column = 1)
+    self.circuits = tk.Button(self.physics_frame, text = "Circuit diagrams", command = self.circuit_diagrams)
+    self.circuits.grid(row = 1, column = 2)
+    go_back = tk.Button(self.physics_frame, text = "Return to main menu", command = lambda(self.return_to_menu(self.physics_frame, "main_menu")))
+    go_back.grid(row = 2, column = 1)
   def chem_menu(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
   def bio_menu(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
   def calculations(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
   def display_constants(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
   def conversions(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
   def data_graphs(self):
-    self.buttonframe.forget()
+    try:
+      self.buttonframe.forget()
+      self.intro.forget()
+    except:
+      pass
+  
+  def return_to_menu(self, frame, menu):
+    frame.forget()
+    returning = {"main_menu": self.main_menu(), "physics_menu": self.physics_menu(), "chem_menu", self.chem_menu, "bio_menu", self.bio_menu}
+    returning[menu]
 gui_calc = Sci_Calculator()
 gui_calc.master.mainloop()
